c12b9a1e14fe1f53d31534943c38544027436fc0
ilias atrar <23261@ma-web.nl>
2019-11-29 09:56:53 +0100

    Uitwerking van de exercise

---
+++ for_klas.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$arrayName = array('Sjonnie de Wiel','Herman Kaal','Henk de Steen','Inge Kerkhoven','ert Kruiswijk');
+for ($i = 0; $i < count($arrayName); $i++)
+
+{
+  echo $arrayName[$i] . "\n";
+}
+
+
+ ?>
